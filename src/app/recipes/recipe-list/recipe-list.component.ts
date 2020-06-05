d54826e13dd038133a91a4333619b5b282ed153d
Bao Nguyen Duy <bao.nguyen.duy@tpssoft.com>
2020-06-05 17:28:55 +0700

    Display recipes and their details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe',
+      'A test recipe',
+      'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX30006678.jpg'
+    ),
+    new Recipe(
+      'Another test recipe',
+      'A test recipe',
+      'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX30006678.jpg'
+    )
+  ];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
